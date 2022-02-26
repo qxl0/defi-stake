9ceb7c36eb58eedafdb6593d1af25e4b6f5fb6da
Qiang <qxl0@yahoo.com>
2022-02-26 16:24:31 -0500

    First test

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -19,7 +19,7 @@ def test_set_price_feed_contract():
     token_farm, dapp_token = deploy_token_farm_and_dapp_token()
     # Act
     price_feed_address=get_contract("eth_usd_price_feed")
-    print(price_feed_address)
+    print(f"price feed is {price_feed_address}")
     token_farm.setPriceFeedContract(
         dapp_token.address, price_feed_address, {"from": account}
     )
@@ -29,3 +29,20 @@ def test_set_price_feed_contract():
         token_farm.setPriceFeedContract(
             dapp_token.address, get_contract("eth_usd_price_feed"), {"from": non_owner}
         )
+  
+def test_stake_tokens(amount_staked):
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # Act
+    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(amount_staked, dapp_token.address, {"from": account})
+    # Assert
+    assert (
+        token_farm.stakingBalance(dapp_token.address, account.address) == amount_staked
+    )
+    assert token_farm.uniqueTokensStaked(account.address) == 1
+    assert token_farm.stakers(0) == account.address
+    return token_farm, dapp_token
